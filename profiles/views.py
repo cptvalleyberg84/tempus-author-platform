578d8124e39373080593ae094f6fc2ca98a79d19
cptvalleyberg84 <cpt.valleyberg@gmail.com>
2025-01-30 21:59:38 +0100

    Update allauth templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
 
 
-# Create your views here.
+@login_required
 def profile(request):
-    """ Display the user's profile """
+    """ Display the user's profile. """
+
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    try:
+        profile = request.user.userprofile
+    except UserProfile.DoesNotExist:
+        # Either create profile automatically
+        profile = UserProfile.objects.create(user=request.user)
+        # Or redirect to profile creation page
+        # return redirect('create_profile')
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
