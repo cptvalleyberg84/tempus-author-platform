2f6c7cf319f3fda03dc00b56df075cafed663772
cptvalleyberg84 <cpt.valleyberg@gmail.com>
2025-02-12 11:54:46 +0100

    Fix admin summernote and missing admin favicon bug

--- tempusap_config/urls.py
+++ tempusap_config/urls.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.views.generic import RedirectView
 
 urlpatterns = [
-    # path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon/favicon.ico')),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),
